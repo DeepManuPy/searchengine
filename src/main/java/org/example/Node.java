f1452f756569731453097d01869ad0bc697f430d
deepmanupy <reddymanudeep@gmail.com>
2022-12-12 14:29:05 -0500

    Implementing Inverted Index search using tries.

---
+++ src/main/java/org/example/Node.java
@@ -0,0 +1,26 @@
+package org.example;
+
+import java.util.HashMap;
+
+public class Node<t>{
+    char key;
+    t value;
+    HashMap<Character, Node<t>> children;
+
+    public Node() {
+        this.children = new HashMap<Character, Node<t>>();
+
+    }
+
+    public Node(char key) {
+        this.key = key;
+        this.children = new HashMap<Character, Node<t>>();
+    }
+	/*
+	public Node(char key, t value) {
+		this.key = key;
+		this.value = value;
+		this.children = new HashMap<Character, Node<t>>();
+	}
+	*/
+}
